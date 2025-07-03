70d11e5a91ac36f723875dacba427d6cfc212613
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-07-03 16:10:05 +0200

    Basic docs structure setted

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,11 @@ author = 'Joan'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    "sphinx.ext.autodoc",
+]
+
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
