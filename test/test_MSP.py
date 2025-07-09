e928f4b50553efa92898f0b4abb25e391545447a
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-07-09 10:09:30 +0200

    First version of iterative MSP solver

---
+++ test/test_MSP.py
@@ -0,0 +1,61 @@
+import pytest
+import numpy as np
+from msptools.MSP import solve_MSP_from_arrays, array_MSP_iterative, array_MSP_inverse
+from msptools.dipole_moments import calculate_dipole_moments_linear, polarizability_to_matrix
+
+np.random.seed(42)
+np.set_printoptions(precision=3, suppress=True)
+
+class Test_MSP_solver_from_arrays:
+    num_particles = 5
+    dimension = 3
+    polarizability = 2.0 + 1.0j
+    external_field = np.random.rand(num_particles, dimension)
+    wave_number = 2.0
+
+    def test_error_handling(self):
+        with pytest.raises(ValueError):
+            solve_MSP_from_arrays(polarizability=self.polarizability, external_field=self.external_field, wave_number=self.wave_number, green_tensor=np.eye(3), method='Unknown')
+    
+    def test_incorrect_green_tensor_shape(self):
+        with pytest.raises(ValueError):
+            solve_MSP_from_arrays(polarizability=self.polarizability, external_field=self.external_field, wave_number=self.wave_number, green_tensor=np.random.rand(3, 3, 2, 2), method='Iterative')
+
+class Test_MSP_iterative:
+    num_particles = 2
+    dimension = 3
+    polarizability = 1.0 + 0.5j
+    external_field = np.random.rand(num_particles, dimension)
+    wave_number = 1.0
+    green_tensor = np.random.rand(num_particles, num_particles, dimension, dimension)\
+        + 1j * np.random.rand(num_particles, num_particles, dimension, dimension)
+    tolerance = 1e-6
+
+    @pytest.mark.parametrize("GT_scale", [5, 2, 1, 0.5])
+    def test_non_convergence_error(self, GT_scale):
+        with pytest.raises(ValueError, match="The new field is significantly larger than the external field, indicating potential divergence in the iterative method."):
+            array_MSP_iterative(self.polarizability, self.external_field, self.wave_number, self.green_tensor * GT_scale, tolerance=self.tolerance)
+        
+    def test_zero_scattering(self):
+        zero_green_tensor = np.zeros((self.num_particles, self.num_particles, self.dimension, self.dimension))
+        total_field = array_MSP_iterative(self.polarizability, self.external_field, self.wave_number, zero_green_tensor, tolerance=self.tolerance)
+        assert np.allclose(total_field, self.external_field), "Total field should equal external field when green tensor is zero."
+    
+    def test_convergence(self):
+        small_green_tensor = 0.1 * self.green_tensor
+        total_field = array_MSP_iterative(self.polarizability, self.external_field, self.wave_number, small_green_tensor, tolerance=self.tolerance)
+        polarizability_matrix = polarizability_to_matrix(self.polarizability, self.num_particles, self.dimension)
+        scattering_matrix = self.wave_number**2 *\
+            small_green_tensor.transpose(0,2,1,3).reshape(self.num_particles * self.dimension, self.num_particles * self.dimension)\
+                @ polarizability_matrix
+        scattering_field = np.zeros_like(self.external_field.flatten(), dtype=np.complex128)
+        total_scattering_matrix = scattering_matrix.copy()
+
+        for i in range(500):
+            scattering_field += total_scattering_matrix @ self.external_field.flatten()
+            total_scattering_matrix = total_scattering_matrix @ scattering_matrix
+
+        expected_field = self.external_field + scattering_field.reshape(self.num_particles, self.dimension)
+
+        assert np.allclose(total_field, expected_field, rtol=self.tolerance), "Total field did not converge to expected value."
+
--- test/test_dipole_moments.py
+++ test/test_dipole_moments.py
@@ -1,6 +1,6 @@
 import pytest
 import numpy as np
-from msptools.dipole_moments import calculate_dipole_moments_linear
+from msptools.dipole_moments import calculate_dipole_moments_linear, polarizability_to_matrix
 
 
 class TestDipoleMomentsLin:
@@ -20,6 +20,33 @@ class TestDipoleMomentsLin:
         with pytest.raises(TypeError):
             calculate_dipole_moments_linear("invalid_type", np.array([[1, 0, 0], [0, 1+2j, -3 + 5j]]))
     
+    def test_different_polarizabilities(self):
+        electric_field = np.array([[1, 0, 0], [0, 1+2j, -3 + 5j], [0, 0, 1]])
+        polarizabilities = [1 + 0j, 2 + 0j, 6j]
+        dipole_moments = calculate_dipole_moments_linear(polarizabilities, electric_field)
+        
+        for i in range(electric_field.shape[0]):
+            assert np.allclose(dipole_moments[i, :], polarizabilities[i] * electric_field[i, :]), \
+                f"Dipole moment for particle {i} should match polarizability times electric field"
+
     
 
+class TestPolarizabilityToMatrix:
+    
+    dimensions = 3 
+    
+    @pytest.mark.parametrize(["polarizability", "num_particles", "expected_shape"], [
+        (1 + 0j, 1, (3, 3)),
+        ([1 + 0j, 2 + 0j, 6j], 3, (9, 9)),
+        (1 + 2j, 3, (9, 9))])
 
+    
+    def test_dimensions(self, polarizability, num_particles, expected_shape):
+        result = polarizability_to_matrix(polarizability, num_particles, self.dimensions)
+        assert result.shape == expected_shape, f"Expected shape {expected_shape}, but got {result.shape}."
+    
+    @pytest.mark.parametrize("num_particles", [1, 2, 3, 5])
+    def test_different_scalar_polarizabilities(self, num_particles):
+        polarizability = np.random.rand(num_particles) + 1j * np.random.rand(num_particles)
+        result = polarizability_to_matrix(polarizability, num_particles, self.dimensions)
+        assert np.allclose(np.diag(result[:self.dimensions, :self.dimensions]), polarizability[0].repeat(self.dimensions)), "Diagonal elements should match the polarizability values."
