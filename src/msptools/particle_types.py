ae2d31bfe90ae3ceef4e29a22f1b406e009a7a0c
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-10-02 13:03:15 +0200

    Simplified test and structure

--- src/msptools/particle_types.py
+++ src/msptools/particle_types.py
@@ -1,22 +1,20 @@
 from .polarizability_mod import calculate_sphere_polarizability
+from typing import List, Tuple, Self
+import numpy as np
 
 class ParticleType:
     """Class representing a type of particle with specific properties."""
 
-    def __init__(self):
-        self.properties = []
+    def __init__(self) -> None:
+        self.properties = {}
+
 
 class SphereType(ParticleType):
     """Class representing spherical particles."""
 
     def __init__(self, radius: float = 1.0, material: str = "default"):
         super().__init__()
-        radius = radius
-        if material == "default":
-            polarizability = 1.0
-        else:
-            polarizability = calculate_sphere_polarizability(radius, material)
         self.properties = {
             "radius": radius,
-            "polarizability": polarizability
+            "material": material
         }
