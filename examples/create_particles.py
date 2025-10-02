69b3df44d48eb1510aabb21e1b2fe16f99c27246
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-10-02 16:43:25 +0200

    Added first creating particles example and modified Types class structure

---
+++ examples/create_particles.py
@@ -0,0 +1,25 @@
+import msptools as msp
+import numpy as np
+
+# Create a type
+type1 = msp.SphereType(radius=2.5, material="wood")
+
+# Create a Particles object with the custom type
+particles = msp.Particles(types=type1)
+print(f"Number of types in the system: {len(particles.types)}")  # Should be 1
+print(f"Particle radius for type1: {particles.types[0].radius}")  # Should show radius of type1
+
+# Create a particle at origin with the current added type
+particles.add_particles([0.0, 0.0, 0.0])
+print(f"\nNumber of particles in the system: {len(particles.positions)}")  # Should be 1
+print(f"Type assignments for particles: {particles.type_assignments}")  # Should show [0]
+
+# Create another type
+type2 = msp.SphereType(radius=1.0, material="plastic")
+# Add particles in the x,y diagonal with the new type
+positions = [[i +1, i+1, 0.0] for i in range(5)]
+particles.add_particles(positions, type=type2)
+print(f"\nNew Number of particles in the system: {len(particles.positions)}")
+print(f"Type assignments for particles: {particles.type_assignments}")  # Should show [0, 1, 1, 1, 1, 1]
+
+
--- src/msptools/particle_types.py
+++ src/msptools/particle_types.py
@@ -5,16 +5,11 @@ import numpy as np
 class ParticleType:
     """Class representing a type of particle with specific properties."""
 
-    def __init__(self) -> None:
-        self.properties = {}
-
 
 class SphereType(ParticleType):
     """Class representing spherical particles."""
 
     def __init__(self, radius: float = 1.0, material: str = "default"):
         super().__init__()
-        self.properties = {
-            "radius": radius,
-            "material": material
-        }
+        self.radius = radius
+        self.material = material
