3ca2fe03a49ebed5b83eddf0b7c666deeba4d6f1
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-10-02 08:22:18 +0200

    Simplyified initialization only with particle type

--- test/test_msptools.py
+++ test/test_msptools.py
@@ -2,11 +2,6 @@ import pytest
 import msptools as msp
 import numpy as np
 
-def test_greet(capsys):
-    msp.greet()
-    captured = capsys.readouterr()
-    assert "Welcome to the msptools package!" in captured.out
-
 
 class TestSystem:
 
@@ -16,10 +11,7 @@ class TestSystem:
         particles = msp.Particles()
         # check that the object is initialized correctly
         assert isinstance(particles, msp.Particles), "The object is not an instance of Particles class"
-
-    def test_initialize_positions(self):
-        positions = np.array([[0, 0, 0], [1, 0, 0], [0, 1, 0]])
-        particles = msp.Particles(positions=positions)
-        assert len(particles.positions) == len(positions), "The number of positions is not correct"
+        assert len(particles.types) == 1, "The default system should have one particle type"
+        assert isinstance(particles.types[0], msp.SphereType), "The default particle type should be SphereType"
 
 
