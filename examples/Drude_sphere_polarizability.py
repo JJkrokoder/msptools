a428220a17c9748b96099139a452dbf79d98672c
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-10-21 12:54:20 +0200

    Updated permittivity Drude tests

--- examples/Drude_sphere_polarizability.py
+++ examples/Drude_sphere_polarizability.py
@@ -2,6 +2,7 @@ import msptools as msp
 import numpy as np
 from scipy.constants import c, hbar, e
 import refractiveindex
+import matplotlib.pyplot as plt
 
 wavelength = 1000e-9
 frequency = c / wavelength * 2 * np.pi * hbar / e
@@ -16,6 +17,8 @@ drude_epsilon = msp.permittivity.permittivity_Drude(frequency=frequency,
                                                           collision_frequency=collision_frequency,
                                                           epsilon_inf=epsilon_inf)
 
+drude_epsilon_tabulated = refractiveindex.RefractiveIndexMaterial(shelf='main', book='Au', page='McPeak').get_epsilon(wavelength_nm=wavelength*1e9)
+
 print()
 print("Drude Model Permittivity Calculation for Gold-like Material")
 print("-----------------------------------")
@@ -28,6 +31,7 @@ print(f"  Collision Frequency: {collision_frequency:.2f} eV")
 print(f"  Collision Wavelength: {collision_wavelength*1e9:.2f} nm")
 print(f"  Epsilon Inf: {epsilon_inf}")
 print(f"Drude permittivity at {wavelength*1e9} nm: {drude_epsilon:.4f}")
+print(f"McPeak's permittivity at {wavelength*1e9} nm: {drude_epsilon_tabulated:.4f}")
 print()
 
 wavelengths = np.linspace(500, 1100, 80)
@@ -37,10 +41,17 @@ permittivities = [msp.permittivity.permittivity_Drude(frequency=freq,
                                                             collision_frequency=collision_frequency,
                                                             epsilon_inf=epsilon_inf) for freq in frequencies]
 
-import matplotlib.pyplot as plt
+                                                            
+Au = refractiveindex.RefractiveIndexMaterial(shelf='main', book='Au', page='McPeak')
+
+tabulated_permittivities = [Au.get_epsilon(wavelength_nm=wavelength) for wavelength in wavelengths]
+
+
 plt.figure(figsize=(8, 6))
-plt.plot(wavelengths, np.real(permittivities), label='Real Part')
-plt.plot(wavelengths, np.imag(permittivities), label='Imaginary Part')
+plt.plot(wavelengths, np.real(permittivities), 'k-', label='Real Part')
+plt.plot(wavelengths, np.imag(permittivities), 'r-', label='Imaginary Part')
+plt.plot(wavelengths, np.real(tabulated_permittivities), 'k--', label='Real Part (Tabulated)')
+plt.plot(wavelengths, np.imag(tabulated_permittivities), 'r--', label='Imaginary Part (Tabulated)')
 plt.title('Drude Model Permittivity for a Gold-like Material', fontsize=14)
 plt.xlabel('Wavelength (nm)', fontsize=12)
 plt.ylabel('Permittivity', fontsize=12)
--- test/test_permittivity.py
+++ test/test_permittivity.py
@@ -1,12 +1,28 @@
 import pytest
 import numpy as np
+from msptools.permittivity import permittivity_Drude
 
 @pytest.mark.parametrize("frequency", [1e2, 1e3, 1e4, 1e5])
 def test_high_frequency_Drude_permittivity(frequency):
-    from msptools.permittivity import permittivity_Drude
     epsilon_inf = 1
     plasma_frequency = 1
     collision_frequency = 0.01
     epsilon_inf = permittivity_Drude(frequency=frequency, plasma_frequency=plasma_frequency, collision_frequency=collision_frequency, epsilon_inf=epsilon_inf)
     assert isinstance(epsilon_inf, complex)
-    assert np.isclose(abs(epsilon_inf - 1), plasma_frequency ** 2 / np.sqrt(frequency**4 + (frequency * collision_frequency)**2), rtol=1e-2)
+    assert np.isclose(abs(epsilon_inf - 1), plasma_frequency ** 2 / np.sqrt(frequency**4 + (frequency * collision_frequency)**2), rtol=1e-12)
+
+class TestDrudePermittivity_parts():
+    frequency = 1
+    plasma_frequency = 1
+    collision_frequency = 0.1
+    epsilon_inf = 5
+    epsilon = permittivity_Drude(frequency=frequency, plasma_frequency=plasma_frequency, collision_frequency=collision_frequency, epsilon_inf=epsilon_inf)
+
+    def test_real_part_Drude_permittivity(self):
+        expected_real = self.epsilon_inf - self.plasma_frequency**2 / (self.frequency**2 + self.collision_frequency**2)
+        assert np.isclose(self.epsilon.real, expected_real, rtol=1e-12), f"Expected real part {expected_real:4f}, got {self.epsilon.real:4f}"
+
+    def test_imaginary_part_Drude_permittivity(self):
+        expected_imag = (self.plasma_frequency**2 * self.collision_frequency) / (self.frequency**3 + self.frequency * (self.collision_frequency)**2)
+        assert np.isclose(self.epsilon.imag, expected_imag, rtol=1e-12), f"Expected imaginary part {expected_imag:4f}, got {self.epsilon.imag:4f}"
+        
