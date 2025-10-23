Fixed units inf field

src/msptools/field_mod.py
@@ -15,9 +15,9 @@ def plane_wave_function(direction: np.ndarray,
         The propagation direction of the plane wave as a 3-element list or array.
         It is assumed to be normalized.
     amplitude :
-        The amplitude of the plane wave.
-    position :
-        The position at which to evaluate the field.
+        The amplitude vector of the plane wave.
+    positions :
+        The positions at which to evaluate the field.
     k_magnitude :
         The magnitude of the wave vector.
 
@@ -33,9 +33,11 @@ def plane_wave_function(direction: np.ndarray,
     where A is the amplitude, k is the wave vector, and r is the position vector
     - positions and k_magnitude should be in consistent units.
     """
+    
     k_vector = direction * k_magnitude
 
-    electric_field = amplitude * np.exp(1j * (k_vector @ positions.T))
+    phase_factors = np.exp(1j * np.dot(positions, k_vector))
+    electric_field = phase_factors[:, np.newaxis] * amplitude.T
     return electric_field
 
 
@@ -71,63 +73,21 @@ class Field:
                 raise ValueError("'frequency' specified but 'frequency_unit' is None.")
             else:
                 self.frequency = frequency_to_eV(frequency, frequency_unit)
+                self.wave_number_um = frequency_to_wavenumber_um(self.frequency)
+                self.wavelength_nm = 2*np.pi*1000/self.wave_number_um
         else:
             if wavelength_unit is None:
                 raise ValueError("'wavelength' specified but 'wavelength_unit' is None.")
             else:  
                 wavelength_nm = wavelength_to_nm(wavelength, wavelength_unit)
+                self.wavelength_nm = wavelength_nm
                 self.frequency = nm_to_eV(wavelength_nm)
-        self.frequency = frequency
-        self.wave_number_um = frequency_to_wavenumber_um(self.frequency)
-        self.wavelength_nm = 2*np.pi*1000/self.wave_number_um
-    
-    def get_field_in_positions(self, positions: np.ndarray) -> np.ndarray:
-        """
-        Method to get the total electric field at specified positions.
-        
-        Parameters
-        ----------
-        positions :
-            The positions at which to evaluate the field.
+                self.wave_number_um = 2*np.pi*1000/self.wavelength_nm
 
-        Returns
-        -------
-        np.ndarray
-            The electric field at the specified positions.
-        """
-        raise NotImplementedError("This method should be implemented by subclasses.")
+    def __str__(self):
+        return f"Field: frequency = {self.frequency:.4f} eV, wavelength = {self.wavelength_nm:.2f} nm"
 
-class PlaneWaveField(Field):
-    """Class representing a plane wave electromagnetic field."""
-    
-    def __init__(self,
-                 direction: List[float] | np.ndarray,
-                 amplitude: np.ndarray,
-                 **kwargs) -> None:
-        """
-        Initialize a PlaneWaveField object by specifying its direction, amplitude and frequency or wavelength.
 
-        Parameters
-        ----------
-        direction :
-            The propagation direction of the plane wave as a 3-element list. It is normalized by default.
-        amplitude :
-            The amplitude of the plane wave.
-        frequency :
-            The frequency of the plane wave.
-        frequency_unit :
-            The unit of the frequency.
-        wavelength :
-            The wavelength of the plane wave.
-        wavelength_unit :
-            The unit of the wavelength.
-        """
-
-        super().__init__(**kwargs)
-        self.amplitude = amplitude
-        self.direction = np.array(direction) / np.linalg.norm(direction)
-        self.external_field_function = None
-    
     def get_external_field_in_positions(self, positions: np.ndarray) -> np.ndarray:
         """
         Method to get the external electric field at specified positions.
@@ -141,4 +101,48 @@ class PlaneWaveField(Field):
         np.ndarray
             The external electric field at the specified positions.
         """
-        return plane_wave_function(self.direction, self.amplitude, positions, self.wave_number_um / 1e3)
+        if self.external_field_function is None:
+            raise NotImplementedError("The method 'get_external_field_in_positions' must be implemented in subclasses.")
+        else:
+            return self.external_field_function(positions)
+    
+
+class PlaneWaveField(Field):
+    """Class representing a plane wave electromagnetic field."""
+    
+    def __init__(self,
+                 direction: List[float] | np.ndarray,
+                 amplitude: float | complex,
+                 polarization: List[float] | np.ndarray,
+                 **kwargs) -> None:
+        """
+        Initialize a PlaneWaveField object by specifying its direction, amplitude and frequency or wavelength.
+
+        Parameters
+        ----------
+        direction :
+            The propagation direction of the plane wave as a 3-element list. It is normalized by default.
+        amplitude :
+            The amplitude of the plane wave.
+        polarization :
+            The polarization vector of the plane wave. It is normalized by default.
+        frequency :
+            The frequency of the plane wave.
+        frequency_unit :
+            The unit of the frequency.
+        wavelength :
+            The wavelength of the plane wave.
+        wavelength_unit :
+            The unit of the wavelength.
+        """
+
+        super().__init__(**kwargs)
+        self.amplitude = amplitude * np.array(polarization) / np.linalg.norm(polarization)
+        self.direction = np.array(direction) / np.linalg.norm(direction)
+        
+        self.external_field_function = lambda positions: plane_wave_function(
+            direction=self.direction,
+            amplitude=self.amplitude,
+            positions=positions,
+            k_magnitude=self.wave_number_um / 1000  # convert from um^-1 to nm^-1
+        )

test/test_msptools.py
@@ -6,7 +6,7 @@ class TestSystem:
     medium_permittivity = 1.0
     
     def test_initialize_system(self):
-        field = msp.PlaneWaveField(direction=[0, 0, 1], frequency=1.0, frequency_unit="eV", amplitude=np.array([1.0, 0.0, 0.0]))
+        field = msp.PlaneWaveField(direction=[0, 0, 1], frequency=1.0, frequency_unit="eV", amplitude= 1.0, polarization=np.array([1.0, 0.0, 0.0]))
         type1 = msp.SphereType(radius=1.0, material="glass")
         system = msp.System(field=field, medium_permittivity=self.medium_permittivity, particle_types=type1, positions_unit="nm")
         
@@ -16,7 +16,7 @@ class TestSystem:
         assert system.particle_types[0].radius == 1.0, "Particle type radius should be initialized to 1.0"
 
     def test_add_particles_single_type(self):
-        field = msp.PlaneWaveField(direction=[0, 0, 1], frequency=1.0, frequency_unit="eV", amplitude=np.array([1.0, 0.0, 0.0]))
+        field = msp.PlaneWaveField(direction=[0, 0, 1], frequency=1.0, frequency_unit="eV", amplitude= 1.0, polarization=np.array([1.0, 0.0, 0.0]))
         type1 = msp.SphereType(radius=1.0, material="glass")
         system = msp.System(field=field, medium_permittivity=self.medium_permittivity, particle_types=[type1], positions_unit="nm")
         