83ccddc1b7904814f2006c6bdb7ebe646e6a578b
Joan Javier Ronquillo Tutiven <joan@ronquillo.ftmc.uam.es>
2025-10-23 11:42:19 +0200

    Fixed example

--- examples/create_particles.py
+++ examples/create_particles.py
@@ -5,7 +5,9 @@ import numpy as np
 type1 = msp.SphereType(radius=2.5, material="wood")
 
 # Create a ParticleData object with the custom type
-system = msp.System(particle_types=type1, field=msp.Field(frequency=1.0))
+system = msp.System(particle_types=type1, field=msp.Field(frequency=1.0,
+                                                          frequency_unit="eV"),
+                                                          positions_unit="nm")
 print(f"Number of types in the system: {len(system.particle_types)}")  # Should be 1
 print(f"Particle radius for type1: {system.particle_types[0].radius}")  # Should show radius of type1
 
