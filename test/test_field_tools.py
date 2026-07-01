Fixed double gradient shape

test/test_field_tools.py
@@ -129,12 +129,11 @@ class Test_Plane_Wave_Double_Gradient():
         amplitude = np.array([1.0, 0.0, 0.0])
         wave_number_nm = 2 * np.pi / 500  # Corresponds to 500 nm wavelength
         positions = np.array([[0.0, 0.0, 0.0],
-                              [0.0, 0.0, 125.0],
                               [0.0, 0.0, 250.0]])
         
         computed_double_gradient = msp.plane_wave_double_gradient(direction, amplitude, positions, wave_number_nm)
 
-        assert computed_double_gradient.shape == (3, 3, 3, 3), f"Expected double gradient shape (3, 3, 3, 3), got {computed_double_gradient.shape}"
+        assert computed_double_gradient.shape == (2, 3, 3, 3), f"Expected double gradient shape (2, 3, 3, 3), got {computed_double_gradient.shape}"
 
        
 class Test_Standing_Wave_Function():